11ea4576862f25ac09fdc67caaec93e009179e88
snowman07 <59627816+snowman07@users.noreply.github.com>
2021-01-12 23:25:24 -0700

    Create Track entity, add annotation, fully immplemented property for nullable string and ? for nullable int

---
+++ ChinookSolution/ChinookSystem/Entities/Track.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespace
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+
+namespace ChinookSystem.Entities
+{
+    [Table("Tracks")]
+    internal class Track
+    {
+        private string _Composer;
+
+        [Key]
+        public int TrackId { get; set; }
+
+        [Required(ErrorMessage = "Track name is required.")]
+        [StringLength(120, ErrorMessage = "Track name is limited to 120 characters.")]
+        public string Name { get; set; }
+
+        public int? AlbumId { get;  set; } // nullable int
+
+        public int MediaTypeId { get; set; }
+
+        public int? GenreId { get; set; } // nullable int
+
+        [StringLength(120, ErrorMessage = "Track name is limited to 120 characters.")]
+        public string Composer 
+        {
+            get { return _Composer; }
+            set { _Composer = string.IsNullOrEmpty(value) ? null : value; } 
+        }
+
+        public int Milliseconds { get; set; }
+
+        public int? Bytes { get; set; } // nullable int
+
+        public int UnitPrice { get; set; }
+    }
+}
