172e4e3a9cbc711bc330fd31f7b05c2823f917eb
snowman07 <59627816+snowman07@users.noreply.github.com>
2021-02-11 10:41:47 -0700

    AlbumController

--- ChinookSolution/ChinookSystem/BLL/AlbumController.cs
+++ ChinookSolution/ChinookSystem/BLL/AlbumController.cs
@@ -90,6 +90,7 @@ namespace ChinookSystem.BLL
                 //  to retrieve the database record on the primary key
                 //  return context.DbSetname.Find(xxx);
                 //---------------------------
+
                 // (...).FirstOrDefault will return either
                 //      a) the first record matching the where condition
                 //      b) a null value
