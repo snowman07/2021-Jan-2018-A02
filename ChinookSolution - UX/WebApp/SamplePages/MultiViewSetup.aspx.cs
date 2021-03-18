3174f9fc71fd84a2fb2bda61645dde3a2ade0ad9
snowman07 <59627816+snowman07@users.noreply.github.com>
2021-03-17 18:02:49 -0600

    multiview discussion DONE

--- ChinookSolution - UX/WebApp/SamplePages/MultiViewSetup.aspx.cs
+++ ChinookSolution - UX/WebApp/SamplePages/MultiViewSetup.aspx.cs
@@ -13,5 +13,53 @@ namespace WebApp.SamplePages
         {
 
         }
+
+        protected void Menu1_MenuItemClick(object sender, MenuEventArgs e)
+        {
+            //to switch views
+            //grab a value from e representing the new view to display
+            int index = Int32.Parse(e.Item.Value);
+
+            //assign the index to ActiveViewIndex
+            MultiView1.ActiveViewIndex = index;
+        }
+
+        protected void SendTo2From1_Click(object sender, EventArgs e)
+        {
+            IODataV2.Text = IODataV1.Text;
+            //can I automatically go to the second view
+            //MultiView1.ActiveViewIndex = 1;
+            //if you alter the page, you should consider altering the menu button also
+            //Menu1.Items[1].Selected = true;
+        }
+
+        protected void SendTo3From1_Click(object sender, EventArgs e)
+        {
+            IODataV3.Text = IODataV1.Text;
+            //can I automatically go to the second view
+            //MultiView1.ActiveViewIndex = 2;
+            //if you alter the page, you should consider altering the menu button also
+            //Menu1.Items[2].Selected = true;
+        }
+
+        protected void SendTo1From2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SendTo3From2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SendTo1From3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SendTo2From3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
